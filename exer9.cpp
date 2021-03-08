64e2473dd7b7d635763f02868c838a2789799fb1
Michael Evangelos Diamantis <michaeldiamantis17@outlook.com>
2021-03-08 22:25:12 +0200

    Completed exercise.

--- exer9.cpp
+++ exer9.cpp
@@ -5,19 +5,19 @@ int main()
 	//Speculation: 32
 	//Speculation: 42....295
 	unsigned u = 10 , u2 = 42;
-	std::cout << u2 - u << std::endl; 
-	std::cout << u - u2 << std::endl; 
+	std::cout << u2 - u << std::endl; //Answer: 32
+	std::cout << u - u2 << std::endl; //Answer: 4294967264
 
 	//Speculation: 32
 	//Speculation: -32
 	int i = 10, i2 = 42;
-	std::cout << i2 - i << std::endl; 
-	std::cout << i - i2 << std::endl; 
+	std::cout << i2 - i << std::endl; //Answer: 32
+	std::cout << i - i2 << std::endl; //Answer: -32
 
 	//Speculation: 0
 	//Speculation: 0
-	std::cout << i - u << std::endl; 
-	std::cout << u - i << std::endl; 
+	std::cout << i - u << std::endl; //Answer: 0
+	std::cout << u - i << std::endl; //Answer: 0
 
 	return 0;
 }
